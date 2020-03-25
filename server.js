61f7b326f3126a92c5a5b5c6af5c0edb904e034d
Xander Dean <aaxelld@gmail.com>
2020-03-25 17:47:39 -0400

    added middleware to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.listen(3000,(req,res)=>{
     console.log("Neighbor Hate Online - Port 3000")
 })
