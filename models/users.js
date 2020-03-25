a5b499f021c686ecf0fc6d01fe7f44209c9eeffb
Xander Dean <aaxelld@gmail.com>
2020-03-25 17:46:00 -0400

    added npm init and finished models schema

--- models/users.js
+++ models/users.js
@@ -1,1 +1,10 @@
-const express= require('express');
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username:{type:String, required:true},
+    password:{type:String, required:true}
+})
+
+const Users = mongoose.model('Users',userSchema);
+
+module.exports = Users;
---
+++ server.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const app = express();
+
+app.listen(3000,(req,res)=>{
+    console.log("Neighbor Hate Online - Port 3000")
+})
