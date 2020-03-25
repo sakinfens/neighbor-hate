d7c0d57467885d8b5c943ffa07e0c519feac168b
Xander Dean <aaxelld@gmail.com>
2020-03-25 18:23:29 -0400

    added mongoose connection with dotenv config files

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express=require('express');
 const app = express();
+const mongoose = require('mongoose')
 
+// MiddleWare
 app.use(express.static('public'));
+
+// Controllers
+
+// CONNECTION
 
 app.listen(3000,(req,res)=>{
     console.log("Neighbor Hate Online - Port 3000")
